d30f68b9980a993e8b78569f804eaf6b9c5a5090
Eugene Prikazchikov <eprikazc@gmail.com>
2017-05-11 16:42:22 +0300

    Implement "Add" button for songs

---
+++ app/routes/bands/band/songs.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+import Song from '../../../models/band';
+
+export default Ember.Route.extend({
+  model: function() {
+    return this.modelFor('bands.band');
+  },
+  actions: {
+    createSong() {
+      let band = this.controller.get('model');
+      let title = this.controller.get('title');
+      let song = Song.create({
+        band: band,
+        title: title,
+      });
+      band.get('songs').pushObject(song);
+      this.controller.set('title', '');
+    },
+  },
+});
