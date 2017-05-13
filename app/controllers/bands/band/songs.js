b0d50bf6747410ac4623a062aac2dc6f9433686b
Eugene Prikazchikov <eprikazc@gmail.com>
2017-05-13 16:07:14 +0300

    Disable Add Song button if text is empty

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  title: '',
+  isAddButtonDisabled: Ember.computed.not('title'),
   actions: {
     updateRating(params) {
       let song = params.item,
