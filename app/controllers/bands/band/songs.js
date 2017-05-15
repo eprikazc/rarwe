f2e2be0ec09043c53f38e33d428cce3c11182816
Eugene Prikazchikov <eprikazc@gmail.com>
2017-05-15 11:27:26 +0300

    Add missing songs controller

---
+++ app/controllers/bands/band/songs.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  title: '',
+  isAddButtonDisabled: Ember.computed.not('title'),
+  hasSongs: Ember.computed.gt('model.songs.length', 0),
+  songCreationStarted: false,
+  canCreateSong: Ember.computed.or(
+    'hasSongs',
+    'songCreationStarted'
+  ),
+  actions: {
+    updateRating(params) {
+      let song = params.item,
+        rating = params.rating;
+      song.set('rating', rating);
+    },
+    enableSongCreation() {
+      this.set('songCreationStarted', true);
+    },
+  },
+});
