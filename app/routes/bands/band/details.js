3cde2dbeb0efb39f1ee9d40d3479805f290da70c
Eugene Prikazchikov <eprikazc@gmail.com>
2017-05-15 11:26:50 +0300

    Add ability to edit band description

--- app/routes/bands/band/details.js
+++ app/routes/bands/band/details.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    willTransition(transition) {
+      if (this.controller.get('isEditing')) {
+        if (window.confirm('You are editing. Are you sure?')) {
+          controller.set('isEditing', false);
+        } else {
+          transition.abort();
+        }
+      }
+    },
+  }
 });
