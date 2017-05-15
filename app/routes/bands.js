314b02f8e09894c6f89165c0749950dceb4967f8
Eugene Prikazchikov <eprikazc@gmail.com>
2017-05-15 14:00:44 +0300

    Convert app models to use ember-data

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -5,44 +5,7 @@ import Song from '../models/song';
 
 export default Ember.Route.extend({
   model: function() {
-    let blackDog = Song.create({
-      title: 'Black Dog',
-      band: 'Led Zeppelin',
-      rating: 3,
-    });
-    let yellowLedbetter = Song.create({
-      title: 'Yellow Ledbetter',
-      band: 'Pearl Jam',
-      rating: 3,
-    });
-    let pretender = Song.create({
-      title: 'The Pretender',
-      band: 'Foo Fighters',
-      rating: 5,
-    });
-    let daughter = Song.create({
-      title: 'Daughter',
-      band: 'Pearl Jam',
-      rating: 5,
-    });
-
-    let ledZeppelin = Band.create({
-      name: 'Led Zeppelin',
-      songs: [blackDog]
-    });
-
-    let pearlJam = Band.create({
-      name: 'Pearl Jam',
-      description: 'Pearl Jam is an American rock band, formed in Seattle, Washington in 1990.',
-      songs: [yellowLedbetter, daughter]
-    });
-
-    let fooFighters = Band.create({
-      name: 'Foo Fighters',
-      songs: [pretender]
-    });
-
-    return [ledZeppelin, pearlJam, fooFighters];
+    return this.store.findAll('band');
   },
   actions: {
     didTransition() {
