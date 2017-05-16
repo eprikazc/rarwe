3e86a1b51f2e6a2a938b94ac517edb73371faaca
Eugene Prikazchikov <eprikazc@gmail.com>
2017-05-16 09:47:58 +0300

    Mock HTTP response in bands acceptance test

--- tests/acceptance/bands-test.js
+++ tests/acceptance/bands-test.js
@@ -1,9 +1,41 @@
 import { test } from 'qunit';
 import moduleForAcceptance from 'rarwe/tests/helpers/module-for-acceptance';
+import Pretender from 'pretender';
 
-moduleForAcceptance('Acceptance | bands');
+
+moduleForAcceptance('Acceptance | bands', {
+  afterEach() {
+    server.shutdown();
+  }
+});
 
 test('List bands', function(assert) {
+  server = new Pretender(function() {
+    this.get('/bands', function() {
+      var response = {
+        data: [{
+            id: 1,
+            type: 'bands',
+            attributes: {
+              name: 'Radiohead'
+            }
+          },
+          {
+            id: 2,
+            type: 'bands',
+            attributes: {
+              name: 'Long Distance Calling'
+            }
+          },
+        ]
+      };
+      return [200, {
+          'Content-Type': 'application/vnd.api+json'
+        },
+        JSON.stringify(response)
+      ];
+    });
+  });
   visit('/bands');
 
   andThen(function() {
